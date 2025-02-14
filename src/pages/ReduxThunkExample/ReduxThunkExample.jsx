a66a9ca1647d133b17faed34a24c1f91cb2c557c
zetra <karanpatadia28@gmail.com>
2025-02-15 01:49:39 +0530

    Adding Debouncing Example to redux-thunk page

--- src/pages/ReduxThunkExample/ReduxThunkExample.jsx
+++ src/pages/ReduxThunkExample/ReduxThunkExample.jsx
@@ -1,35 +1,75 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { debounce } from 'lodash';
+
 import { fetchUsers, selectUsers } from './usersSlice';
 
 const ReduxThunkExample = () => {
   const dispatch = useDispatch();
   const { loading, users, error } = useSelector(selectUsers);
+  const [search, setSearch] = useState('');
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
   useEffect(() => {
     if (users.length === 0) dispatch(fetchUsers());
-  }, [dispatch]);
+  }, [dispatch, users.length]);
+
+  const onChangeHandler = useCallback(
+    debounce((search) => {
+      setSearch(search);
+    }, 500),
+    []
+  );
+
+  useEffect(() => {
+    // Filter users when search term changes
+    if (search) {
+      setFilteredUsers(
+        users.filter((user) =>
+          user.name.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredUsers(users);
+    }
+  }, [search, users]);
 
   return (
     <div className='flex flex-col justify-center items-center'>
       <h1 className='text-3xl font-bold mb-3 border-b-4 w-full text-center'>
         ReduxThunkExample
-        <div className='flex flex-col'>
-          <div className='flex flex-col justify-center items-center border-2 border-b-sky-700 p-5 gap-5'>
-            <h2 className='text-2xl w-full border-b-2 text-center'>
-              User List
-            </h2>
-            <div>
-              {loading && <div>Loading...</div>}
-              {error && <div>Error...</div>}
-              {users &&
-                users.map((user) => {
-                  return <li key={user.id}>{user.name}</li>;
-                })}
-            </div>
+      </h1>
+
+      <div className='flex flex-col'>
+        <div className='flex flex-col justify-center items-center border-2 border-b-sky-700 p-5 gap-5'>
+          <h2 className='text-2xl w-full border-b-2 text-center'>User List</h2>
+          <div>
+            {loading && <div>Loading...</div>}
+            {error && <div>Error...</div>}
+            {filteredUsers &&
+              filteredUsers.map((user) => {
+                return <li key={user.id}>{user.name}</li>;
+              })}
           </div>
         </div>
-      </h1>
+
+        {/* Deboucing Example */}
+        <div className='flex flex-col justify-center items-center border-2 border-b-sky-700 p-5 gap-5'>
+          <h2 className='text-2xl w-full border-b-2 text-center'>
+            Debouncing Example
+          </h2>
+          <div>
+            <input
+              type='text'
+              onChange={(e) => {
+                onChangeHandler(e.target.value);
+              }}
+              placeholder='Search users...'
+              className='p-2 border rounded-md'
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
