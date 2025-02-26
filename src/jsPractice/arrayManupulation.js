f3cfefcba3fa594a1365f574b9e7d8a014415566
zetra <karanpatadia28@gmail.com>
2025-02-26 23:12:15 +0530

    adding js practice

---
+++ src/jsPractice/arrayManupulation.js
@@ -0,0 +1,213 @@
+/*
+What is the difference between .splice() and .slice()?
+*/
+
+// How do you remove duplicates from an array?
+console.log('Remove Duplicates Output');
+const a1 = [1, 2, 3, 3, 2];
+
+const b = [...new Set(a1)];
+console.log('using set: ', b);
+let uniques = a1.filter((num, index) => a1.indexOf(num) === index);
+const seen = {};
+uniques = a1.filter((num) => !seen[num] && (seen[num] = true));
+console.log('using filer', uniques);
+console.log('......................');
+
+//How do you find the maximum and minimum values in an array?
+console.log('Find max min');
+const a2 = [1, 4, 6, 8];
+
+let max = Math.max(...a2);
+let min = Math.min(...a2);
+console.log('using inbuilt Math.max, Math.min', max, min);
+max = a2[0];
+min = a2[0];
+a2.forEach((num) => {
+  if (num > max) max = num;
+  if (num < max) min = num;
+});
+console.log('using forEach', max, min);
+console.log('......................');
+
+// How do you check if an array contains a specific element?
+console.log('Find specific elment');
+const a3 = [1, 2, 3, 4];
+console.log(a3.indexOf(3));
+console.log('......................');
+
+// How do you reverse an array without modifying the original one?
+console.log('Reverse Array without modiying ');
+const a4 = [1, 2, 3, 4];
+let revArr = a4.reduce((acc, num) => [num, ...acc], []);
+revArr = a4.map((_, i) => a4[a4.length - i - 1]);
+console.log(revArr);
+console.log('......................');
+
+// What is the difference between .splice() and .slice()?
+console.log('Slice vs Splice');
+let a5 = [1, 2, 3, 4, 5];
+
+// splice(start, deleteCount, itemsToInsert...)
+a5.splice(1, 2); // Removes 2 elements starting from index 1 → arr = [1, 4, 5]
+console.log(a5);
+a5 = [1, 2, 3, 4, 5];
+// slice(start, end) - does not modify original
+const sliced = a5.slice(1, 3); // [4, 5]
+console.log(sliced);
+console.log('......................');
+
+// How do you flatten a nested array (e.g., [1, [2, [3, 4]], 5] → [1, 2, 3, 4, 5])?
+
+const a6 = [1, [2, [3, 4]], 5];
+
+const flatArr1 = a6.flat(Infinity);
+console.log('Using flat method: ', flatArr1);
+
+const flattenArrays = (arr, depth) => {
+  if (depth === -1) return [];
+
+  return arr.reduce((acc, num) => {
+    if (Array.isArray(num)) {
+      return acc.concat(flattenArrays(num, depth - 1));
+    } else {
+      return acc.concat(num);
+    }
+  }, []);
+};
+console.log('Using reduce method: ', flattenArrays(a6, 3));
+console.log('......................');
+
+// How do you find the intersection of two arrays?
+let a7 = [1, 2, 3, 5, 4];
+let a8 = [2, 3, 4];
+
+const setArr = new Set(a8);
+const intersaction = a7.filter((num) => setArr.has(num));
+
+console.log('Intersaction between [1, 2, 3] and [2, 3, 4]: ', intersaction);
+
+console.log('......................');
+
+// How do you group an array of objects by a property?
+
+console.log('How do you group an array of objects by a property?');
+const people = [
+  { name: 'Alice', age: 25 },
+  { name: 'Bob', age: 25 },
+  { name: 'Charlie', age: 30 },
+];
+
+const ageObj = people.reduce((acc, person) => {
+  const { age } = person;
+
+  if (!acc[age]) acc[age] = [];
+  acc[age].push(person);
+  return acc;
+}, {});
+console.log(ageObj);
+
+console.log('......................');
+
+// How do you find the missing number in an array of consecutive numbers?
+
+console.log(
+  'How do you find the missing number in an array of consecutive numbers?'
+);
+const a9 = [1, 2, 4, 5];
+
+const len = a9.length;
+const expectedSum = ((len + 1) * (len + 2)) / 2;
+const actualSum = a9.reduce((acc, num) => acc + num);
+console.log('Missing Num: ', expectedSum - actualSum);
+console.log('......................');
+
+// How do you find the missing numbers in an array of consecutive numbers?
+
+console.log(
+  'How do you find the missing numbers in an array of consecutive numbers?'
+);
+const a10 = [1, 2, 5, 6, 9];
+const missingNums = [];
+let index = 0;
+
+for (let i = a10[0]; i <= a10[a10.length - 1]; i++) {
+  if (i !== a10[index]) {
+    missingNums.push(i);
+  } else {
+    index++;
+  }
+}
+
+console.log('Missing Numbers: ', missingNums);
+console.log('......................');
+
+// How do you rotate an array k times to the right (e.g., [1,2,3,4,5] → [4,5,1,2,3] for k=2)?
+
+console.log(
+  'How do you rotate an array k times to the right (e.g., [1,2,3,4,5] → [4,5,1,2,3] for k=2)?'
+);
+const b1 = [1, 2, 3, 4, 5];
+let k = 8;
+const b1Len = b1.length;
+k = k % b1Len;
+
+const slice1 = b1.slice(0, -k);
+const slice2 = b1.slice(-k, b1Len);
+
+const rotateArr = [...slice2, ...slice1];
+console.log(`Rotating to right by ${k} steps: [${rotateArr}]`);
+
+console.log('......................');
+
+// How do you check if two arrays are equal?
+
+const b2 = [1, 2, 3, 4, 5];
+const b3 = [1, 2, 3, 4, 5];
+
+if (b2.length !== b3.length) console.log('Not Equal');
+const check = b2.every((num, index) => num === b3[index]);
+console.log('Equal Arrays', check);
+
+console.log('......................');
+// Given an array of numbers, move all zeroes to the end while keeping order.
+console.log(
+  'Given an array of numbers, move all zeroes to the end while keeping order.'
+);
+
+const b4 = [1, 2, 0, 0, 5, 6];
+
+const moveZeros = (arr) => {
+  const arr2 = [];
+  const arr3 = [];
+
+  arr.forEach((num) => {
+    num === 0 ? arr2.push(num) : arr3.push(num);
+  });
+  return [...arr3, ...arr2];
+};
+
+console.log(moveZeros(b4));
+
+console.log('......................');
+
+//Find the First Non-Repeating Element
+console.log('Find the First Non-Repeating Element');
+
+const b5 = [1, 2, 3, 3, 4, 1];
+
+const findFirstUnique = (arr) => {
+  const count = {};
+
+  arr.forEach((num) => {
+    count[num] = (count[num] || 0) + 1;
+  });
+
+  return arr.find((num) => count[num] === 1);
+};
+
+console.log(findFirstUnique(b5));
+
+console.log('......................');
+
+console.log('......................');
---
+++ src/jsPractice/stringManipulations.js
@@ -0,0 +1,315 @@
+// How do you reverse a string without using .reverse()?
+console.log('Reverse a string without using .reverse()');
+
+const s1 = 'abc';
+const revStr1 = s1.split('').reduce((acc, char) => char + acc, '');
+console.log(revStr1);
+console.log('......................');
+
+// How do you check if a string is a palindrome?
+
+let s2 = 'abccba';
+const revStr2 = s2.split('').reduce((acc, char) => char + acc, '');
+console.log('Palindrome: ', revStr2 === s2);
+console.log('......................');
+
+// How do you count the occurrences of each character in a string?
+console.log('How do you count the occurrences of each character in a string?');
+
+let s3 = 'aabcc';
+const countStr = s3.split('').reduce((acc, char) => {
+  acc[char] = acc[char] || 0 + 1;
+  return acc;
+}, {});
+
+console.log(countStr);
+console.log('......................');
+
+// How do you check if two strings are anagrams?
+let s4 = 'listen';
+let s5 = 'silent';
+
+const sortMethod = (str) => {
+  return str.split('').sort().join('');
+};
+
+console.log('Anagrams: ', sortMethod(s4) === sortMethod(s5));
+
+console.log('......................');
+
+// How do you extract numbers from a given string?
+let s6 = 'ab255y44';
+const extractNum = s6.match(/\d+/g).map(Number);
+console.log('Numbers: ', extractNum);
+
+console.log('......................');
+
+console.log('How do you replace all occurrences of a substring in a string?');
+let s7 = 'Hello world';
+const newStr = s7.replaceAll('Hello', 'World');
+const newStr2 = s7.replace(/Hello/g, 'World');
+console.log('Using ReplaceAll: ' + newStr);
+console.log('Using Replace and regex: ' + newStr2);
+console.log('......................');
+
+//How do you find the first non-repeating character in a string?
+console.log('How do you find the first non-repeating character in a string?');
+
+const s8 = 'abbc';
+const countChar = {};
+for (const char of s8) {
+  countChar[char] = (countChar[char] || 0) + 1;
+}
+for (const char of s8) {
+  if (countChar[char] === 1) {
+    console.log('First non repearing character: ', char);
+    break;
+  }
+}
+console.log('......................');
+
+// How do you count the number of vowels in a string?
+console.log('How do you count the number of vowels in a string?');
+
+const s9 = 'abceede';
+const vowelsArray = s9.match(/[aeiou]/gi);
+console.log('Count of vowels: ', vowelsArray.length);
+
+// How do you find the longest word in a string?
+console.log('How do you find the longest word in a string?');
+
+const st1 = 'Today is Cloudy';
+const st1Arr = st1.split(' ');
+console.log(st1Arr);
+
+const longestWord = st1Arr.reduce(
+  (longest, word) => (word.length > longest.length ? word : longest),
+  ''
+);
+console.log(longestWord);
+
+console.log('......................');
+
+// How do you capitalize the first letter of each word in a string?
+console.log('How do you capitalize the first letter of each word in a string?');
+
+const st2 = 'today is cloudy';
+const st2Arr = st2.split(' ');
+
+const res2 = st2Arr
+  .map((word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  })
+  .join(' ');
+console.log('Capital first letter of words: ', res2);
+console.log('......................');
+
+// How do you remove duplicate characters from a string?
+console.log('How do you remove duplicate characters from a string?');
+
+const st3 = 'aabc';
+
+const res3 = [...new Set(st3.split(''))].join('');
+
+console.log(res3);
+console.log('......................');
+
+//How do you count the occurrences of each character in a string?
+console.log('How do you count the occurrences of each character in a string?');
+
+const st4 = 'aabvcc';
+const st4Arr = st4.split('');
+
+const countChars = st4Arr.reduce((acc, char) => {
+  acc[char] = (acc[char] || 0) + 1;
+  return acc;
+}, {});
+console.log(countChars);
+
+console.log('......................');
+
+// How do you find the longest substring without repeating characters?
+console.log(
+  'How do you find the longest substring without repeating characters?'
+);
+
+const st5 = 'abccbad';
+
+/*
+abccbad
+abc
+while -bcc -> cc -> c
+cb
+cba
+cbad
+*/
+const lengthSub = (s) => {
+  let maxLength = 0;
+  const charSet = new Set();
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
+    }
+    charSet.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength;
+};
+
+console.log(lengthSub(st5));
+
+console.log('......................');
+
+//How do you reverse the words in a sentence but keep their order?
+console.log('How do you reverse the words in a sentence but keep their order?');
+
+const st6 = 'Hello World';
+
+const st6Arr = st6.split(' ');
+const rev6Str = (str) => {
+  return str.split('').reduce((acc, char) => char + acc, '');
+};
+console.log(st6Arr.map(rev6Str).join(' '));
+console.log('......................');
+
+//How do you check if a string contains only unique characters?
+console.log('How do you check if a string contains only unique characters?');
+
+const st7 = 'helo';
+let check = true;
+
+st7.split('').forEach((char, index) => {
+  if (st7.indexOf(char) !== index) check = false;
+});
+console.log('Unique Characher? ', check);
+
+console.log('......................');
+
+// How do you find the most frequent character in a string?
+console.log('How do you find the most frequent character in a string?');
+
+const st8 = 'banana';
+
+const maxCharFind = (str) => {
+  const count = {};
+  let maxChar = '';
+  let maxCount = 0;
+
+  for (let char of str) {
+    count[char] = (count[char] || 0) + 1;
+    if (count[char] > maxCount) {
+      maxCount = count[char];
+      maxChar = char;
+    }
+  }
+  return `${maxChar}: ${maxCount}`;
+};
+console.log(maxCharFind(st8));
+
+console.log('......................');
+
+//How do you check if one string is a rotation of another?
+console.log('How do you check if one string is a rotation of another?');
+
+const st9 = 'abcd';
+const st10 = 'cdab';
+
+const checkIfRotated = (str1, str2) => {
+  return str1.length === str2.length && (str1 + str1).includes(str2);
+};
+
+console.log(checkIfRotated(st9, st10));
+
+console.log('......................');
+
+//How do you compress a string using character counts?
+console.log('How do you compress a string using character counts?');
+
+const ns1 = 'aaabbcaabbccccdddooo';
+
+const compressStr = (str) => {
+  let compressed = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      compressed += str[i] + count;
+      count = 0;
+    }
+  }
+  return compressed.length < str.length ? compressed : str;
+};
+console.log(compressStr(ns1));
+console.log('......................');
+
+// How do you find all substrings of a given string?
+console.log('How do you find all substrings of a given string?');
+
+const ns2 = 'abc';
+// a, ab, abc, b, bc, c
+
+const findAllSubs = (str) => {
+  const len = str.length;
+  const totalSubs = (len * (len + 1)) / 2;
+  let left = 0;
+  let right = 1;
+  const subs = [];
+
+  for (let i = 1; i <= totalSubs; i++) {
+    const sub = str.substring(left, right);
+    subs.push(sub);
+
+    if (right === len) {
+      left++;
+      right = left + 1;
+    } else {
+      right++;
+    }
+  }
+  return subs;
+};
+const findAllSubs2 = (str) => {
+  const subs = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j <= str.length; j++) {
+      subs.push(str.substring(i, j));
+    }
+  }
+  return subs;
+};
+
+console.log(findAllSubs(ns2));
+console.log('......................');
+
+// How do you find the longest common prefix among an array of strings?
+//Example: ['flower', 'flow', 'flight'] → "fl"
+console.log(
+  'How do you find the longest common prefix among an array of strings?'
+);
+
+const ns3 = ['flower', 'flow', 'fight'];
+
+const findLongestCommonPrefix = (arr) => {
+  if (!arr.length) return '';
+
+  let prefix = arr[0];
+
+  for (let word of arr) {
+    while (!word.startsWith(prefix)) {
+      prefix = prefix.slice(0, -1);
+      if (prefix === '') return '';
+    }
+  }
+  return prefix;
+};
+
+console.log(findLongestCommonPrefix(ns3));
+console.log('......................');
+
+console.log('......................');
+console.log('......................');
